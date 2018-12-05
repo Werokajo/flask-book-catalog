08143e2f2a52b5941d31227ac08c35c89fbc03f3
Werokajo <werner@hintis.at>
2018-12-05 09:47:17 +0100

    prod/dev Kommentat

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from app.auth.models import User
 from sqlalchemy import exc
 
-deploy_type = 'prod'
+deploy_type = 'prod' # 'dev' 'prod' jeweils austauschen vor Deployment to production
 
 flask_app = create_app(deploy_type)
 with flask_app.app_context():
